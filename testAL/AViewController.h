825b917f231c735aad095daa8af100e092b40b82
Gal Schwimmer <galsc@YanivCohen.local>
2014-12-30 13:18:16 +0200

    first commit

---
+++ testAL/AViewController.h
@@ -0,0 +1,6 @@
+
+#import <UIKit/UIKit.h>
+
+@interface AViewController : UIViewController
+
+@end
---
+++ testAL/ViewController.m
@@ -0,0 +1,33 @@
+
+
+#import "ViewController.h"
+#import "AViewController.h"
+//#import "UIView+AutoLayout.h"
+#import "PureLayout.h"
+
+@interface ViewController ()
+@property (strong) AViewController *aVC;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.aVC = [[AViewController alloc] init];
+    self.aVC.view.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:self.aVC.view];
+    [self.aVC.view autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:0];
+    [self.aVC.view autoSetDimension:ALDimensionHeight toSize:100];
+    [self.aVC.view autoSetDimension:ALDimensionWidth toSize:320];
+    NSLayoutConstraint *constraint = [self.aVC.view autoPinEdgeToSuperviewEdge:ALEdgeBottom withInset:-100];
+    [self.view layoutIfNeeded];
+    [UIView animateWithDuration:2 animations:^{
+        constraint.constant = -25;
+        [self.view layoutIfNeeded];
+    }];
+    
+}
+
+
+@end
